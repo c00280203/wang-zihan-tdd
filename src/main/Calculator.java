ac2886bef3eab345157a29b741d2878fdf895cd0
Zihan Wang <C00280203@setu.ie>
2025-10-17 14:33:17 +0100

    Implement subtract

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b; // Minimal implementation to pass the specific test
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
